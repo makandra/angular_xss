1e67a4d62de1e8e317bf9a8f5de1e73559fbf37a
Henning Koch <henning.koch@makandra.de>
2014-01-04 18:28:58 +0100

    add tests ensuring no creative way of expressing double curlies with html entities will evade escaping

--- lib/angular_xss/escaper.rb
+++ lib/angular_xss/escaper.rb
@@ -1,5 +1,14 @@
 module AngularXss
   class Escaper
+    
+    #BRACE = [
+    #  '\\{',
+    #  '&lcub;',
+    #  '&lbrace;',
+    #  '&#x0*7b;',
+    #  '&#0*123;',
+    #]
+    #DOUBLE_BRACE_REGEXP = Regexp.new("(#{BRACE.join('|')})(#{BRACE.join('|')})", Regexp::IGNORECASE)
 
     def self.escape(string)
       string.gsub('{{', ' { { ')
--- spec/shared/support/engine_preventing_angular_xss.rb
+++ spec/shared/support/engine_preventing_angular_xss.rb
@@ -1,10 +1,41 @@
 shared_examples_for 'engine preventing Angular XSS' do
 
-  it 'escapes Angular interpolation marks iff a string is unsafe' do
-    engine = respond_to?(:view) ? view : template
-    html = engine.render(partial)
+  let(:engine) { respond_to?(:view) ? view : template }
+
+  let(:html) { engine.render(partial) }
+
+  it 'escapes Angular interpolation marks in unsafe strings' do
+    html.should_not include('{{unsafe}}')
+    html.should include(' { { unsafe}}')
+  end
+
+  it 'recognizes the many ways to express an opening curly brace in HTML' do
+
     html.should include(" { { unsafe}}")
     html.should_not include("{{unsafe}}")
+
+    braces = [
+     '{',
+     '&lcub;',
+     '&lbrace;',
+     '&#x7b;',
+     '&#X7B;',
+     '&#x000007b;',
+     '&#x000000000007b;',
+     '&#123;',
+     '&#000000123;',
+     '&#0000000000000123;'
+    ]
+
+    braces.each do |brace1|
+      braces.each do |brace2|
+        html.should_not include("#{brace1}#{brace2}unsafe}}")
+      end
+    end
+
+  end
+
+  it 'does not escape Angular interpolation marks in safe strings' do
     html.should include("{{safe}}")
     html.should_not include(" { { safe}}")
   end
